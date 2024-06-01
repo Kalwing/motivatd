c898147916b74932bb11f860e96d9984ee30640f
blueh0rse <110935049+blueh0rse@users.noreply.github.com>
2024-06-01 03:58:35 +0200

    read page source code on button click

--- popup.js
+++ popup.js
@@ -5,3 +5,29 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+// Create text button
+document.addEventListener("DOMContentLoaded", function () {
+  var button = document.getElementById("btnText");
+  button.addEventListener("click", function () {
+    console.log("Click: btnText");
+    source_code = get_page_source_code();
+    console.log(source_code);
+  });
+});
+
+// Create email button
+document.addEventListener("DOMContentLoaded", function () {
+  var button = document.getElementById("btnEmail");
+  button.addEventListener("click", function () {
+    console.log("Click: btnEmail");
+    source_code = get_page_source_code();
+    console.log(source_code);
+  });
+});
+
+// Get source code
+function get_page_source_code() {
+  console.log("Func: get_page_source_code");
+  return document.documentElement.outerHTML;
+}
